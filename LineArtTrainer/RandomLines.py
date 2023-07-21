f1cfd6916d3c41471f83a6b92e1371b26febe14d
Zikkamen <jeffreyntue@gmail.com>
2023-07-21 11:29:29 +0200

    Refactored drawing random lines and added a separate picture for viewing

--- LineArtTrainer/RandomLines.py
+++ LineArtTrainer/RandomLines.py
@@ -1,5 +1,6 @@
 import random
 
+import PIL.ImageDraw
 import numpy as np
 from PIL import ImageDraw, ImageFont, Image
 
@@ -8,27 +9,27 @@ class RandomLines:
     def __int__(self) -> None:
         pass
 
-    def draw_random_lines(self, img_task: Image, img_answer: Image) -> None:
+    def draw_random_lines(self, img_task: Image, img_task_view: Image, img_answer: Image) -> None:
         draw_task = ImageDraw.Draw(img_task)
+        draw_task_view = ImageDraw.Draw(img_task_view)
         draw_answer = ImageDraw.Draw(img_answer)
 
         for i in range(10):
-            x1 = random.randint(0, 2480)
-            y1 = random.randint(0, 3580)
-            x2 = random.randint(0, 2480)
-            y2 = random.randint(0, 3580)
-
+            pos1 = (random.randint(0, 2480), random.randint(0, 3580))
+            pos2 = (random.randint(0, 2480), random.randint(0, 3580))
             color = np.random.randint(50, 255, size=3)
 
-            x_length = 20
-
-            draw_task.line((x1 + x_length, y1 + x_length, x1 - x_length, y1 - x_length), fill=tuple(color), width=10)
-            draw_task.line((x1 - x_length, y1 + x_length, x1 + x_length, y1 - x_length), fill=tuple(color), width=10)
-            draw_task.line((x2 + x_length, y2 + x_length, x2 - x_length, y2 - x_length), fill=tuple(color), width=10)
-            draw_task.line((x2 - x_length, y2 + x_length, x2 + x_length, y2 - x_length), fill=tuple(color), width=10)
+            self.draw_crosses(draw_task, pos1, pos2, 20, color)
+            self.draw_crosses(draw_task_view, pos1, pos2, 20, color)
 
             font = ImageFont.truetype("arial.ttf", 75)
-            draw_task.text((x1, y1), str(i + 1), (50, 50, 50), font)
-            draw_task.text((x2, y2), str(i + 1), (50, 50, 50), font)
+            draw_task_view.text(pos1, str(i + 1), (50, 50, 50), font)
+            draw_task_view.text(pos2, str(i + 1), (50, 50, 50), font)
 
-            draw_answer.line((x1, y1, x2, y2), fill=(0, 0, 0), width=10)
+            draw_answer.line(pos1 + pos2, fill=(0, 0, 0), width=10)
+
+    def draw_crosses(self, canvas: PIL.ImageDraw.ImageDraw, pos1: tuple, pos2: tuple, length: int, color: tuple):
+        canvas.line((pos1[0] + length, pos1[1] + length, pos1[0] - length, pos1[1] - length), fill=tuple(color), width=10)
+        canvas.line((pos1[0] - length, pos1[1] + length, pos1[0] + length, pos1[1] - length), fill=tuple(color), width=10)
+        canvas.line((pos2[0] + length, pos2[1] + length, pos2[0] - length, pos2[1] - length), fill=tuple(color), width=10)
+        canvas.line((pos2[0] - length, pos2[1] + length, pos2[0] + length, pos2[1] - length), fill=tuple(color), width=10)
