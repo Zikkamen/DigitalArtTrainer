6df822a81a41f1717e272fe09eab753e17158e46
Zikkamen <jeffreyntue@gmail.com>
2023-07-26 19:02:52 +0200

    Added support for html files and added template files

--- webinterface.py
+++ webinterface.py
@@ -1,11 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from typing import Union
 
 app = FastAPI()
+templates = Jinja2Templates(directory="Webinterface/templates")
+app.mount("/static", StaticFiles(directory="Webinterface/static"), name="static")
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+
+@app.get("/", response_class=HTMLResponse)
+async  def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 
 @app.get("/items/{item_id}")
