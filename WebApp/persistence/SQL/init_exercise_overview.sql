844e4058eb6ed3fc1be2881e21b91fd60dacbba0
Zikkamen <jeffreyntue@gmail.com>
2023-08-06 18:15:01 +0200

    Added Images for Categories

--- WebApp/persistence/SQL/init_exercise_overview.sql
+++ WebApp/persistence/SQL/init_exercise_overview.sql
@@ -2,27 +2,27 @@ INSERT INTO exercises_overview(exercise_name, exercise_url, image_url, exercise_
 (
     '3D Training',
     '3DRealm',
-    'exercises/3DRealm.png',
+    'images/exercises/3DRealm.png',
     'Trainer to improve your understanding of the 3 Dimensions'
 );
 INSERT INTO exercises_overview(exercise_name, exercise_url, image_url, exercise_description) VALUES
 (
     'Color Matching Training',
     'ColorMatcher',
-    'exercises/ColorMatcher.png',
+    'images/exercises/ColorMatcher.png',
     'Trainer to see how good your understanding of the colours is'
 );
 INSERT INTO exercises_overview(exercise_name, exercise_url, image_url, exercise_description) VALUES
 (
     'Light Shadow Training',
     'LightShadow',
-    'exercises/LightShadow.png',
+    'images/exercises/LightShadow.png',
     'Trainer to make up your Lighting skills against Blender'
 );
 INSERT INTO exercises_overview(exercise_name, exercise_url, image_url, exercise_description) VALUES
 (
     'Line Art Training',
     'LineArt',
-    'exercises/LineArt.png',
+    'images/exercises/LineArt.png',
     'Trainer to make up your Lighting skills against Blender'
 );
